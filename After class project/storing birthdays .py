f20f9a2f2389863442b1c756315b1c4904d1052a
Arshika2015 <yashvardhansewhag2009@gmail.com>
2025-06-28 12:35:08 +0530

    storing birthdays .py

---
+++ After class project/storing birthdays .py
@@ -0,0 +1,10 @@
+zoya = "3/02/2015"
+anusha = "20/12/2014"
+cherry = "27/04/2015"
+arshika = "27/01/2015"
+jaynika = "16/09/2014"
+print("birthday of zoya is ",zoya)
+print("birthday of anusha is",anusha)
+print("birthday of cherry is ",cherry)
+print("birthday of arshika is ", arshika)
+print("birthday of jaynika is ",jaynika)
