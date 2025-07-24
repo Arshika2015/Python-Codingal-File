45a2f20b2048f91b019bb4f3db6bbc25a6e3a048
Arshika@2015 <yashvardhansewhag2009@gmail.com>
2025-07-24 18:36:02 +0530

    Lesson 17

---
+++ Lesson 17/Pass.py
@@ -0,0 +1,12 @@
+for i in range(20):
+    if i%20 ==0:
+        print("i is divisible by 20 ")
+    elif i%15 ==0:
+        pass
+    elif i%5 ==0:
+        print("i is divisible by 5")
+    elif i%3 ==0:
+        print("i is divisible 3")
+    else:
+        print(i)
+    
