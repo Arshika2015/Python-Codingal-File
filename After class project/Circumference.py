bfca06eb16929b732014d07d1fdd42c4acc94c81
Arshika@2015 <yashvardhansewhag2009@gmail.com>
2025-07-28 18:51:59 +0530

    Afterclass project

---
+++ After class project/Circumference.py
@@ -0,0 +1,7 @@
+import math
+def circumference():
+ radius = int(input ("Enter the radius of the circle"))
+ area = math.pi*(radius)
+ return area
+
+print("the circumference of circle is",circumference())
--- After class project/Circunference.py
+++
@@ -1,4 +0,0 @@
-import math;
-radius = int(input ("Enter the radius of the circle"))
-area = math.pi*("radius radius")
-print("''") 
