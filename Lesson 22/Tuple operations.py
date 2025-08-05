f6d1a07b554d3af6869f2b53a07db07408f8b835
Arshika@2015 <yashvardhansewhag2009@gmail.com>
2025-08-05 18:44:47 +0530

    Lesson 22

---
+++ Lesson 22/Tuple operations.py
@@ -0,0 +1,16 @@
+tuple1 = ("tuple,false,3.2,4")
+print(tuple1)
+tuple2 = (4,6,2,8,3,1)
+print(tuple2)
+tuple2 = tuple2+(9,)
+print(tuple2)
+tuple3 = (4,4,5,6,7,8,8,7,6,5,9)
+print(tuple3.count(9))
+tuple4 = (2,3,4,5,6,7,8,9,10)
+slice1 = tuple4[5:8]
+print(slice1)
+slice2 = tuple4[:7]
+print(slice2)
+
+
+
