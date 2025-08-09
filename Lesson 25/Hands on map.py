97d3193b297bf5ba12bb5b0001830adcc3bfa81d
Arshika@2015 <yashvardhansewhag2009@gmail.com>
2025-08-09 08:41:28 +0530

    Lesson 25

---
+++ Lesson 25/Hands on map.py
@@ -0,0 +1,12 @@
+numbers1 = [1,2,3]
+numbers2 = [4,5,6]
+result = map(lambda x,y:x+y,numbers1,numbers2)
+print(list(result))
+numbs1 = [1,2,3,4,5]
+def sq (n):
+   return n*n
+numbs2 = list(map(sq,numbs1))
+print(numbs2)
+
+
+
