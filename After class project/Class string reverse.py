f01bef687ab51d43bec678b463cd173614e10ee8
Arshika@2015 <yashvardhansewhag2009@gmail.com>
2025-08-21 19:39:54 +0530

    Afterclass project

---
+++ After class project/Class string reverse.py
@@ -0,0 +1,18 @@
+class StringReverser:
+    def reverse_words(self, input_string):
+        """
+        Reverses the order of words in a given string.
+
+        Args:
+            input_string (str): The string to be reversed word by word.
+
+        Returns:
+            str: The string with words reversed.
+        """
+        words = input_string.split()  # Split the string into a list of words
+        reversed_words = reversed(words)  # Reverse the order of words in the list
+        return ' '.join(reversed_words)  # Join the reversed words back into a string with spaces
+reverser = StringReverser()
+my_string = "This is a sample string"
+reversed_result = reverser.reverse_words(my_string)
+print(reversed_result)
